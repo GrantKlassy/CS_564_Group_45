a2a77e3f168eb298dfcddeaec56431a093692bbb
carleyreardon3 <32401321+carleyreardon3@users.noreply.github.com>
2020-02-20 16:24:27 -0600

    create.sql and load.txt from part D

---
+++ PP1/create.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS Items;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Categories;
+DROP TABLE IF EXISTS Bids;
+
+CREATE TABLE Items (
+    itemID INTEGER NOT NULL,
+    name CHAR(100) NOT NULL,
+    currently CHAR(100) NOT NULL,
+    buyPrice CHAR(100),
+    firstBid CHAR(100) NOT NULL,
+    numberOfBids INTEGER NOT NULL,
+    started CHAR(100) NOT NULL,
+    ends CHAR(100) NOT NULL,
+    description CHAR(100) NOT NULL,
+    sellerUserID INTEGER NOT NULL,
+    PRIMARY KEY (itemID)
+);
+
+CREATE TABLE Users (
+    userID CHAR(100) NOT NULL,
+    rating INTEGER NOT NULL,
+    location CHAR(100),
+    country CHAR(100),
+    PRIMARY KEY (userID)
+);
+
+CREATE TABLE Categories (
+    category CHAR(100) NOT NULL,
+    itemID INTEGER NOT NULL,
+    PRIMARY KEY (category, itemID)
+);
+
+CREATE TABLE Bids (
+    itemID INTEGER NOT NULL,
+    userID CHAR(100) NOT NULL,
+    bidTime CHAR(100) NOT NULL,
+    amount CHAR(100) NOT NULL,
+    PRIMARY KEY (itemID, userID, bidTime)
+);
