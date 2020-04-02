ef47ccc42068de255e57bfd4b20f712b54b807d9
Grant Klassy <gklassy@wisc.edu>
2020-04-01 19:56:38 -0500

    Docs and indenting

--- PP2/BufMgr/docs/search/functions_5.js
+++ PP2/BufMgr/docs/search/functions_5.js
@@ -6,5 +6,5 @@ var searchData=
   ['filename',['filename',['../classbadgerdb_1_1_file_exists_exception.html#ac18efb4a600c843c3c6e3a20c91914a6',1,'badgerdb::FileExistsException::filename()'],['../classbadgerdb_1_1_file_not_found_exception.html#a42c86bdb20d593145171122b8d7625c0',1,'badgerdb::FileNotFoundException::filename()'],['../classbadgerdb_1_1_file_open_exception.html#ad5758410a91fe3c9014967f3ce4b8f48',1,'badgerdb::FileOpenException::filename()'],['../classbadgerdb_1_1_invalid_page_exception.html#ab2da9c8f8ccfd57ff34718fdf545ca2f',1,'badgerdb::InvalidPageException::filename()'],['../classbadgerdb_1_1_file.html#af16040a60f18c99305348077ac9e8aa8',1,'badgerdb::File::filename()']]],
   ['filenotfoundexception',['FileNotFoundException',['../classbadgerdb_1_1_file_not_found_exception.html#a53d52b9ecbd214bb5db7e9b3401248b8',1,'badgerdb::FileNotFoundException']]],
   ['fileopenexception',['FileOpenException',['../classbadgerdb_1_1_file_open_exception.html#af315ef5b756efe579a1b8ee7f6e4d352',1,'badgerdb::FileOpenException']]],
-  ['flushfile',['flushFile',['../classbadgerdb_1_1_buf_mgr.html#acc61d1985720411ebb76e70f702827d3',1,'badgerdb::BufMgr']]]
+  ['flushfile',['flushFile',['../classbadgerdb_1_1_buf_mgr.html#a0fa71349d885cba40a01de9ad0829a28',1,'badgerdb::BufMgr']]]
 ];
